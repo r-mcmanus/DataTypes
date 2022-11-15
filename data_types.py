ee139f1f3ed5c3b8919fb70a74b76a88b91122c6
mcman <mcmanus452@gmail.com>
2022-11-15 15:51:32 -0500

    Initial commit

---
+++ data_types.py
@@ -0,0 +1,23 @@
+# integer
+an_int = 42
+
+# float
+a_float = 42.0
+
+# string
+a_string = '42'
+
+# list
+a_list = [4, 2]
+
+#multidimensional list
+multi_list = [[4], [2]]
+
+#dictionary
+a_dict = {'four': 4, 'two': 2}
+
+# tuple
+a_tuple = (4, 2)
+
+
+
